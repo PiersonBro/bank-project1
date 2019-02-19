0743fecb847f21f6609887a152c4b4404901bf0e
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-02-18 18:54:24 -0800

    Create Account.h

---
+++ headers/Account.h
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+#ifndef Account_h
+#define Account_h
+
+#include "Customer.h"
+#include "Transaction.h"
+
+class Account {
+private:
+	Customer* customer;
+	int balance;
+	int accountNum;
+	Transaction* TransArray[];
+
+public:
+	double getBalance();
+	Customer getCustomer();
+	string to_string();   // what does this do ?
+	void setCustomer(Customer* newcustomer);
+	void setBalance(int newbalance);
+
+
+};
+#endif

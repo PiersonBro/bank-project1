1a86e536b3edbf7b0001e6f455cd2469beb6f3d5
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-02-18 18:55:10 -0800

    Create CheckingsAccount.h

---
+++ headers/CheckingsAccount.h
@@ -0,0 +1,10 @@
+#ifndef CheckingsAccount_h
+#define CheckingsAccount_h
+#include "Account.h"
+
+class CheckingsAccount {
+public:
+	double deposit(Transaction transaction);
+	double withdraw(Transaction transaction2);
+	double addInterest(Transaction transaction3);
+#endif 

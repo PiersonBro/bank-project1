92bba92086505e4cda1b06eec8805858b4b6d4dd
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-02-18 18:52:05 -0800

    Create SavingsAccounth.cpp

---
+++ source/SavingsAccounth.cpp
@@ -0,0 +1,16 @@
+#include "SavingsAccounth.h"
+
+double SavingsAccount::deposit(Transaction transaction)
+{
+	return transaction.getAmount();
+}
+
+double SavingsAccount::withdraw(Transaction transaction2)
+{
+	return transaction2.getAmount();
+}
+
+double SavingsAccount::addInterest(Transaction transaction3)
+{
+	return 0;
+}
